5e7d0a1a4a479be83a8b77a26e153cd38009f559
torbenvanassche <torbenvanassche.anime@gmail.com>
2021-10-22 13:48:33 +0200

    Update mesh-library.ts

--- libs/renderer/src/classes/mesh-library.ts
+++ libs/renderer/src/classes/mesh-library.ts
@@ -1,3 +1,34 @@
+import { ReplaySubject } from 'rxjs';
+import { Group } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+interface MeshData {
+    mesh?: THREE.Group;
+    url: string;
+    name: string;
+  }
+
 export class MeshLibrary {
-    data: Map<string, THREE.Mesh> = new Map<string, THREE.Mesh>();
+  data: Set<MeshData> = new Set<MeshData>();
+
+  load(name: string, url: string) {
+    var subject = new ReplaySubject<Group>();
+
+    var array = Array.from(this.data).filter((x) => x.url === url);
+    if (array.length == 0) {
+      new GLTFLoader().load(
+        url,
+        (gltf) => {
+          this.data.add({ mesh: gltf.scene, url: url, name: name });
+          subject.next(gltf.scene);
+        },
+        undefined,
+        (e) => {
+          console.log(e);
+        }
+      );
+    } else {
+      subject.next(array[0].mesh);
+    }
+  }
 }
